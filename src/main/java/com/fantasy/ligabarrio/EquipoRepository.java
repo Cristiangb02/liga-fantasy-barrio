092030687125a46de1a9bce4649bb9a079e26aa4
Cristiangb02 <149502044+Cristiangb02@users.noreply.github.com>
2025-12-23 01:41:22 +0100

    Update EquipoRepository.java

--- src/main/java/com/fantasy/ligabarrio/EquipoRepository.java
+++ src/main/java/com/fantasy/ligabarrio/EquipoRepository.java
@@ -6,9 +6,8 @@ import java.util.List;
 
 @Repository
 public interface EquipoRepository extends JpaRepository<Equipo, Long> {
-    // Esto nos vendrá genial para sacar la clasificación: "Dame todos los equipos de la Jornada 1"
+    
     List<Equipo> findByJornada(Jornada jornada);
-
-    // O para ver el historial de un amigo: "Dame todos los equipos que ha hecho Pepito"
     List<Equipo> findByUsuario(Usuario usuario);
+
 }
