0f88a4eb55e9598cf891a9e25371d57021e51757
Cristiangb02 <cristiangb2002h@gmail.com>
2025-12-30 12:53:32 +0100

    Añadidos los precios de mercado (más correctos, claro)

--- src/main/java/com/fantasy/ligabarrio/InicializadorDatos.java
+++ src/main/java/com/fantasy/ligabarrio/InicializadorDatos.java
@@ -17,10 +17,10 @@ public class InicializadorDatos implements CommandLineRunner {
     private final ActuacionRepository actuacionRepository;
     private final NoticiaRepository noticiaRepository;
 
-    public InicializadorDatos(JugadorRepository jugadorRepository, 
-                              TemporadaRepository temporadaRepository, 
-                              JornadaRepository jornadaRepository, 
-                              UsuarioRepository usuarioRepository, 
+    public InicializadorDatos(JugadorRepository jugadorRepository,
+                              TemporadaRepository temporadaRepository,
+                              JornadaRepository jornadaRepository,
+                              UsuarioRepository usuarioRepository,
                               EquipoRepository equipoRepository,
                               ActuacionRepository actuacionRepository,
                               NoticiaRepository noticiaRepository) {
@@ -36,7 +36,7 @@ public class InicializadorDatos implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        System.out.println(">>> 🔄 CHECKING DATA...");
+        System.out.println(">>> 🔄 CARGANDO JUGADORES (DEFENSAS UNIFICADOS)...");
 
         // 1. TEMPORADA
         if (temporadaRepository.count() == 0) {
@@ -44,98 +44,74 @@ public class InicializadorDatos implements CommandLineRunner {
             temporadaRepository.save(t2026);
         }
 
-        // 2. JUGADORES
         List<Jugador> lista = new ArrayList<>();
-        
-        // --- PORTEROS ---
 
-        lista.add(new Jugador("Aitor", "PORTERO", 1_000_000, "/user.png"));
-        lista.add(new Jugador("Carlos", "PORTERO", 8_130_000, "/carlos.png"));
-        lista.add(new Jugador("Carmelo", "PORTERO", 1_760_000, "/user.png"));
-        lista.add(new Jugador("Cristian", "PORTERO", 6_150_000, "/cristian.png"));
-        lista.add(new Jugador("Diego", "PORTERO", 4_510_000, "/diego.png"));
-        lista.add(new Jugador("Fran", "PORTERO", 2_580_000, "/franportero.png"));
-        lista.add(new Jugador("Jhona", "PORTERO", 14_500_000, "/jhona.png"));
-        lista.add(new Jugador("Juanlu", "PORTERO", 3_850_000, "/juanlu.png"));
-        lista.add(new Jugador("Sergio", "PORTERO", 6_570_000, "/sergio.png"));
+        // --- PORTEROS ---
+        lista.add(new Jugador("Carlos", "PORTERO", 0, 6.8, "/carlos.png"));
+        lista.add(new Jugador("Carmelo", "PORTERO", 0, 6.26, "/carmelo.png"));
+        lista.add(new Jugador("Cristian", "PORTERO", 24, 7.26, "/cristian.png"));
+        lista.add(new Jugador("Diego", "PORTERO", 0, 7.23, "/diego.png"));
+        lista.add(new Jugador("Fran", "PORTERO", 53, 6.47, "/franportero.png"));
+        lista.add(new Jugador("Jhona", "PORTERO", 48, 8.06, "/jhona.png"));
+        lista.add(new Jugador("Juanlu", "PORTERO", 56, 5.98, "/juanlu.png"));
+        lista.add(new Jugador("Sergio", "PORTERO", 0, 6.56, "/sergio.png"));
 
         // --- DEFENSAS ---
-        lista.add(new Jugador("Cardenas", "DEFENSA", 2_370_000, "/cardenas.png"));
-        lista.add(new Jugador("Chico", "DEFENSA", 1_760_000, "/user.png")); 
-        lista.add(new Jugador("Conce", "DEFENSA", 2_380_000, "/conce.png"));
-        lista.add(new Jugador("Diego", "DEFENSA", 14_840_000, "/diego.png")); 
-        lista.add(new Jugador("Javi", "DEFENSA", 3_280_000, "/javier.png"));
-        lista.add(new Jugador("Javier M.", "DEFENSA", 2_260_000, "/javierm.png"));
-        lista.add(new Jugador("Jose", "DEFENSA", 14_070_000, "/jose.png"));
-        lista.add(new Jugador("Juanlu", "DEFENSA", 3_700_000, "/juanlu.png"));
-        lista.add(new Jugador("Mario", "DEFENSA", 5_260_000, "/mario.png"));
-        lista.add(new Jugador("Miguel", "DEFENSA", 2_200_000, "/user.png"));
-        lista.add(new Jugador("Primo", "DEFENSA", 11_410_000, "/primo.png")); 
-        lista.add(new Jugador("Andrés", "DEFENSA", 1_340_000, "/andres.png"));
-        lista.add(new Jugador("Álvaro", "DEFENSA", 5_670_000, "/user.png"));
-        lista.add(new Jugador("Cárdenas", "DEFENSA", 2_380_000, "/cardenas.png")); 
-        lista.add(new Jugador("Chico", "DEFENSA", 3_560_000, "/user.png")); 
-        lista.add(new Jugador("Conce", "DEFENSA", 5_260_000, "/conce.png")); 
-        lista.add(new Jugador("Cristian", "DEFENSA", 8_950_000, "/cristian.png"));
-        lista.add(new Jugador("Diego", "DEFENSA", 6_570_000, "/diego.png")); 
-        lista.add(new Jugador("Javier M", "DEFENSA", 1_290_000, "/javierm.png")); 
-        lista.add(new Jugador("Javi", "DEFENSA", 1_360_000, "/javier.png")); 
-        lista.add(new Jugador("Jhona", "DEFENSA", 3_560_000, "/jhona.png"));
-        lista.add(new Jugador("Jose", "DEFENSA", 3_180_000, "/jose.png")); 
-        lista.add(new Jugador("Juan", "DEFENSA", 1_530_000, "/juan.png"));
-        lista.add(new Jugador("Juanlu", "DEFENSA", 3_880_000, "/juanlu.png")); 
-        lista.add(new Jugador("Lucas", "DEFENSA", 3_700_000, "/lucas.png"));
-        lista.add(new Jugador("Luis", "DEFENSA", 4_200_000, "/luis.png"));
-        lista.add(new Jugador("Manuel hijo Luis", "DEFENSA", 5_760_000, "/user.png"));
-        lista.add(new Jugador("Mario", "DEFENSA", 6_110_000, "/mario.png")); 
-        lista.add(new Jugador("Paco", "DEFENSA", 4_510_000, "/paco.png"));
-        lista.add(new Jugador("Primo", "DEFENSA", 6_340_000, "/primo.png")); 
+
+        lista.add(new Jugador("Alejandro", "DEFENSA", 32, 6.63, "/alejandro.png"));
+        lista.add(new Jugador("Alejandro G.", "DEFENSA", 0, 6.58, "/alejandrogarrocho.png"));
+        lista.add(new Jugador("Andrés", "DEFENSA", 0, 4.72, "/andres.png"));
+        lista.add(new Jugador("Cardenas", "DEFENSA", 45, 6.20, "/cardenas.png"));
+        lista.add(new Jugador("Chico", "DEFENSA", 0, 5.79, "/user.png"));
+        lista.add(new Jugador("Conce", "DEFENSA", 0, 5.84, "/conce.png"));
+        lista.add(new Jugador("Cristian", "DEFENSA", 24, 7.08, "/cristian.png"));
+        lista.add(new Jugador("Diego", "DEFENSA", 0, 8.07, "/diego.png"));
+        lista.add(new Jugador("Javier", "DEFENSA", 0, 6.06, "/javier.png"));
+        lista.add(new Jugador("Javier M.", "DEFENSA", 54, 5.88, "/javierm.png"));
+        lista.add(new Jugador("Jesús Jr", "DEFENSA", 25, 6.62, "/jesusjr.png"));
+        lista.add(new Jugador("Jhona", "DEFENSA", 48, 6.29, "/jhona.png"));
+        lista.add(new Jugador("Jose", "DEFENSA", 0, 7.03, "/jose.png"));
+        lista.add(new Jugador("Juan", "DEFENSA", 45, 6.04, "/juan.png"));
+        lista.add(new Jugador("Juanlu", "DEFENSA", 56, 6.89, "/juanlu.png"));
+        lista.add(new Jugador("Lucas", "DEFENSA", 0, 6.50, "/lucas.png"));
+        lista.add(new Jugador("Luis", "DEFENSA", 57, 6.19, "/luis.png"));
+        lista.add(new Jugador("Mario", "DEFENSA", 59, 7.01, "/mario.png"));
+        lista.add(new Jugador("Paco", "DEFENSA", 0, 7.11, "/paco.png"));
+        lista.add(new Jugador("Primo", "DEFENSA", 0, 6.87, "/primo.png"));
+        lista.add(new Jugador("Sergio", "DEFENSA", 0, 6.98, "/sergio.png"));
 
         // --- MEDIOS ---
-        lista.add(new Jugador("Alberto", "MEDIO", 9_320_000, "/alberto.png"));
-        lista.add(new Jugador("Álvaro", "MEDIO", 5_670_000, "/user.png"));
-        lista.add(new Jugador("Alberto hijo Javier", "MEDIO", 12_850_000, "/user.png"));
-        lista.add(new Jugador("Alejandro", "MEDIO", 9_320_000, "/alejandro.png"));
-        lista.add(new Jugador("Alejandro G.", "MEDIO", 8_650_000, "/alejandrogarrocho.png"));
-        lista.add(new Jugador("Cardenas", "MEDIO", 5_890_000, "/cardenas.png")); 
-        lista.add(new Jugador("Conce", "MEDIO", 8_590_000, "/conce.png")); 
-        lista.add(new Jugador("Cristian", "MEDIO", 4_870_000, "/cristian.png")); 
-        lista.add(new Jugador("David", "MEDIO", 3_030_000, "/user.png"));
-        lista.add(new Jugador("Diego", "MEDIO", 1_890_000, "/diego.png")); 
-        lista.add(new Jugador("Felipe", "MEDIO", 7_860_000, "/felipe.png"));
-        lista.add(new Jugador("Jesús Jr", "MEDIO", 15_380_000, "/jesusjr.png"));
-        lista.add(new Jugador("Jose", "MEDIO", 2_380_000, "/jose.png")); 
-        lista.add(new Jugador("Lucas", "MEDIO", 3_850_000, "/lucas.png")); 
-        lista.add(new Jugador("Luis", "MEDIO", 5_500_000, "/luis.png")); 
-        lista.add(new Jugador("Manuel hijo Luis", "MEDIO", 9_320_000, "/user.png")); 
-        lista.add(new Jugador("Oswaldo", "MEDIO", 4_510_000, "/oswi.png"));
-        lista.add(new Jugador("Pepe", "MEDIO", 3_850_000, "/pepe.png"));
-        lista.add(new Jugador("Primo", "MEDIO", 2_380_000, "/primo.png")); 
-        lista.add(new Jugador("Sebas", "MEDIO", 21_470_000, "/user.png"));
-        lista.add(new Jugador("Sergio", "MEDIO", 17_250_000, "/sergio.png"));
+        lista.add(new Jugador("Alberto", "MEDIO", 0, 8.06, "/alberto.png"));
+        lista.add(new Jugador("Alejandro", "MEDIO", 32, 8.16, "/alejandro.png"));
+        lista.add(new Jugador("Alejandro G.", "MEDIO", 0, 8.52, "/alejandrogarrocho.png"));
+        lista.add(new Jugador("Cardenas", "MEDIO", 45, 6.8, "/cardenas.png"));
+        lista.add(new Jugador("Conce", "MEDIO", 0, 7.13, "/conce.png"));
+        lista.add(new Jugador("Cristian", "MEDIO", 24, 7.66, "/cristian.png"));
+        lista.add(new Jugador("Felipe", "MEDIO", 31, 7.69, "/felipe.png"));
+        lista.add(new Jugador("Jesús Jr", "MEDIO", 25, 7.53, "/jesusjr.png"));
+        lista.add(new Jugador("Lucas", "MEDIO", 0, 5.85, "/lucas.png"));
+        lista.add(new Jugador("Luis", "MEDIO", 57, 7.21, "/luis.png"));
+        lista.add(new Jugador("Oswaldo", "MEDIO", 45, 8.19, "/oswi.png"));
+        lista.add(new Jugador("Pepe", "MEDIO", 67, 6.97, "/pepe.png"));
+        lista.add(new Jugador("Sebas", "MEDIO", 33, 8.13, "/user.png"));
+        lista.add(new Jugador("Sergio", "MEDIO", 0, 8.88, "/sergio.png"));
 
         // --- DELANTEROS ---
-        lista.add(new Jugador("Alberto", "DELANTERO", 5_460_000, "/alberto.png")); 
-        lista.add(new Jugador("Cristian", "DELANTERO", 3_560_000, "/cristian.png")); 
-        lista.add(new Jugador("Jesus", "DELANTERO", 4_870_000, "/jesus.png"));
-        lista.add(new Jugador("Jesús Jr", "DELANTERO", 6_570_000, "/jesusjr.png")); 
-        lista.add(new Jugador("Jhona", "DELANTERO", 3_560_000, "/jhona.png")); 
-        lista.add(new Jugador("Jose", "DELANTERO", 2_800_000, "/jose.png")); 
-        lista.add(new Jugador("Juanlu", "DELANTERO", 2_380_000, "/juanlu.png")); 
-        lista.add(new Jugador("Pepe", "DELANTERO", 5_260_000, "/pepe.png")); 
-        lista.add(new Jugador("Raúl", "DELANTERO", 6_570_000, "/user.png"));
+        lista.add(new Jugador("Cristian", "DELANTERO", 24, 7.96, "/cristian.png"));
+        lista.add(new Jugador("Jesus", "DELANTERO", 42, 7.53, "/jesus.png"));
+        lista.add(new Jugador("Jhona", "DELANTERO", 48, 6.16, "/jhona.png"));
+        lista.add(new Jugador("Juan", "DELANTERO", 45, 5.97, "/juan.png"));
+        lista.add(new Jugador("Juanlu", "DELANTERO", 56, 5.89, "/juanlu.png"));
+        lista.add(new Jugador("Pepe", "DELANTERO", 67, 7.02, "/pepe.png"));
 
-        // 🔴 LÓGICA CORREGIDA: Buscamos por NOMBRE + POSICIÓN
+        //INSERCIÓN SEGURA (Evita duplicados)
         for (Jugador j : lista) {
-            // "Dame todos los jugadores que se llamen 'Diego' Y jueguen de 'DEFENSA'"
+            // Buscamos por nombre y posición exacta
             List<Jugador> existentes = jugadorRepository.findByNombreAndPosicion(j.getNombre(), j.getPosicion());
-            
+
             if (existentes.isEmpty()) {
-                // Si no existe esa combinación exacta, lo creamos
                 jugadorRepository.save(j);
-                System.out.println("✅ NUEVO JUGADOR: " + j.getNombre() + " (" + j.getPosicion() + ")");
-            } else {
-                // Si ya existe (aunque sea un clon), no creamos más
+                System.out.println("✅ NUEVO JUGADOR: " + j.getNombre() + " (" + j.getPosicion() + ") -> " + j.getValor() + "€");
             }
         }
 
@@ -152,7 +128,7 @@ public class InicializadorDatos implements CommandLineRunner {
             usuarioRepository.save(admin);
             System.out.println("👑 ADMIN CREADO");
         }
-        
-        System.out.println(">>> ✅ CARGA DE DATOS INTELIGENTE COMPLETADA.");
+
+        System.out.println(">>> ✅ CARGA DE DATOS COMPLETADA.");
     }
 }
--- src/main/java/com/fantasy/ligabarrio/Jugador.java
+++ src/main/java/com/fantasy/ligabarrio/Jugador.java
@@ -10,45 +10,76 @@ public class Jugador {
     private Long id;
 
     private String nombre;
-    private String posicion; // Es un String: "PORTERO", "DEFENSA"...
+    private String posicion;
+    private int edad;   // NUEVO
+    private double media; // NUEVO (La nota del 0 al 10)
+
     private int valor;
-    private int clausula;
-    private int puntosAcumulados;
-    private String urlImagen;
+    private String imagen;
 
     @ManyToOne
     private Usuario propietario;
-    
+
+    private int puntosAcumulados;
+    private int clausula;
     private Long jornadaFichaje;
 
     public Jugador() {
     }
 
-    public Jugador(String nombre, String posicion, int valor, String urlImagen) {
+    // CONSTRUCTOR NUEVO: Ya no pide "valor", pide "edad" y "media"
+    public Jugador(String nombre, String posicion, int edad, double media, String imagen) {
         this.nombre = nombre;
-        this.posicion = posicion; // Recibe String
-        this.valor = valor;
-        this.clausula = valor; 
+        this.posicion = posicion;
+        this.edad = edad;
+        this.media = media;
+        this.imagen = imagen;
         this.puntosAcumulados = 0;
-        this.urlImagen = urlImagen;
         this.jornadaFichaje = 0L;
+
+        // 🧮 CÁLCULO AUTOMÁTICO DE PRECIO f(x,y)
+        this.valor = calcularPrecioInicial(edad, media);
+        this.clausula = this.valor;
     }
 
+    // LA FÓRMULA MATEMÁTICA
+    private int calcularPrecioInicial(int edad, double media) {
+        // 1. Base exponencial según la media (ajusta el 150000 o el 1.65 si quieres precios más altos/bajos)
+        double precioBase = 150_000 * Math.pow(1.65, media);
+
+        // 2. Factor de edad (Jóvenes valen más, veteranos menos)
+        // 27 es la edad "neutra". Cada año de diferencia es un +/- 1.5%
+        double factorEdad = 1.0 + ((27.0 - edad) * 0.015);
+
+        // 3. Cálculo final
+        double precioFinal = precioBase * factorEdad;
+
+        // 4. Mínimo 150k y redondeo a bonitos (multiplos de 10.000)
+        if (precioFinal < 150_000) precioFinal = 150_000;
+        return (int) (Math.round(precioFinal / 10000.0) * 10000);
+    }
+
+    // --- GETTERS Y SETTERS ---
     public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
-    public String getPosicion() { return posicion; } // Devuelve String
+    public String getPosicion() { return posicion; }
     public void setPosicion(String posicion) { this.posicion = posicion; }
+    public int getEdad() { return edad; }
+    public void setEdad(int edad) { this.edad = edad; }
+    public double getMedia() { return media; }
+    public void setMedia(double media) { this.media = media; }
     public int getValor() { return valor; }
     public void setValor(int valor) { this.valor = valor; }
-    public int getClausula() { return clausula; }
-    public void setClausula(int clausula) { this.clausula = clausula; }
-    public int getPuntosAcumulados() { return puntosAcumulados; }
-    public void setPuntosAcumulados(int puntosAcumulados) { this.puntosAcumulados = puntosAcumulados; }
-    public String getUrlImagen() { return urlImagen; }
-    public void setUrlImagen(String urlImagen) { this.urlImagen = urlImagen; }
+    public String getUrlImagen() { return imagen; } // Mantenemos getUrlImagen para compatibilidad con tu HTML
+    public void setUrlImagen(String imagen) { this.imagen = imagen; }
     public Usuario getPropietario() { return propietario; }
     public void setPropietario(Usuario propietario) { this.propietario = propietario; }
+    public int getPuntosAcumulados() { return puntosAcumulados; }
+    public void setPuntosAcumulados(int puntosAcumulados) { this.puntosAcumulados = puntosAcumulados; }
+    public int getClausula() { return clausula; }
+    public void setClausula(int clausula) { this.clausula = clausula; }
     public Long getJornadaFichaje() { return jornadaFichaje; }
     public void setJornadaFichaje(Long jornadaFichaje) { this.jornadaFichaje = jornadaFichaje; }
 }
