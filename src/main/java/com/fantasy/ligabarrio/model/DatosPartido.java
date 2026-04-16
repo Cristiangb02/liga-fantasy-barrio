974686a675e4c401584449313206763253f87e27
Cristian <cristiangb2002h@gmail.com>
2026-04-16 19:15:03 +0200

    Otro más...

--- src/main/java/com/fantasy/ligabarrio/model/DatosPartido.java
+++ src/main/java/com/fantasy/ligabarrio/model/DatosPartido.java
@@ -1,13 +1,13 @@
-package com.fantasy.ligabarrio.model;
+    package com.fantasy.ligabarrio.model;
 
-public class DatosPartido {
-    public Long idJugador;
-    public Long idJornada;
-    public boolean jugado;
-    public boolean victoria;
-    public boolean derrota;
-    public int goles;
-    public int golesEncajados;
-    public int autogoles;
-    public String colorEquipo;
-}
+    public class DatosPartido {
+        public Long idJugador;
+        public Long idJornada;
+        public boolean jugado;
+        public boolean victoria;
+        public boolean derrota;
+        public int goles;
+        public int golesEncajados;
+        public int autogoles;
+        public String colorEquipo;
+    }
